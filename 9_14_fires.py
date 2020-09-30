1a818735198e8162c57d36d62eceeee6bcb8038c
wscheffe <will.scheffe@gmail.com>
2020-09-30 15:37:01 -0500

    good

---
+++ 9_14_fires.py
@@ -0,0 +1,42 @@
+import json 
+in_file = open('US_fires_9_14.json', 'r') 
+fire_data = json.load(in_file)
+
+brights,lons,lats = [],[],[]
+
+for fires in fire_data:
+    if fires['brightness'] > 450:
+        bright = fires['brightness']
+        brights.append(bright)
+        lon = fires['longitude']
+        lat = fires['latitude']
+        lons.append(lon)
+        lats.append(lat)
+
+import plotly
+from plotly.graph_objs import Scattergeo, Layout
+
+from plotly import offline
+
+fire_data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'marker': {
+        'color': brights,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Brightness'},
+    },
+}]
+
+graph_layout = Layout(
+    title='US Fires - 9/14/2020 through 9/20/2020',
+    geo = dict(
+        projection_scale=5.8,
+        center=dict(lat=36.7783, lon=-115.4179)
+        ),
+    )
+fig = {'data':fire_data, 'layout':graph_layout}
+
+offline.plot(fig, filename='fires_9_14.html')
